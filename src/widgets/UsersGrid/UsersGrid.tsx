bddd04e13b4f4293267afccb55d60eee8f2102d2
Новиков Эмиль <novikovemil@MacBook-Pro-Novikov.local>
2024-04-28 13:49:09 -0700

    [Feat] responsive grid

--- src/widgets/UsersGrid/UsersGrid.tsx
+++ src/widgets/UsersGrid/UsersGrid.tsx
@@ -2,18 +2,19 @@ import React from 'react';
 import { UserCard } from '@/entities/user';
 import { useQuery } from '@tanstack/react-query';
 import { IUser } from '@/shared';
+import { Grid } from './styles';
 
 export const UsersGrid: React.FC = () => {
   const query = useQuery<any>({ queryKey: ['users'] });
 
   return (
-    <div>
+    <Grid>
       {query.data?.data.results?.map((user: IUser) =>
         <UserCard
           user={user}
           key={new Date(user?.registered?.date).getTime()}
         />
       )}
-    </div>
+    </Grid>
   )
 }
---
+++ src/widgets/UsersGrid/styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Grid = styled.div`
+    display: grid;
+    gap: 1rem;
+    grid-template-columns: repeat(auto-fit, 332px);
+`;
