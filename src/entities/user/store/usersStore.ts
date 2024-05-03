791d432788c44661dd1ec9f8e0d97dc5cd5ec691
Новиков Эмиль <novikovemil@MacBook-Pro-Novikov.local>
2024-05-02 23:40:00 -0700

    [Fix] delete

--- src/entities/user/store/usersStore.ts
+++ src/entities/user/store/usersStore.ts
@@ -15,12 +15,21 @@ class UsersState {
   }
 
   deleteUser(email: string) {
-    console.log(email);
-    this.users = this.users.filter((user) => email !== user.email);
-    console.log(this.users);
+    if (this.searchedUsers.length > 0) {
+      this.searchedUsers = this.searchedUsers.filter((user) => email !== user.email);
+      return
+    }
+    if (this.searchedUsers.length === 0) {
+      this.users = this.users.filter((user) => email !== user.email);
+      return
+    }
   }
 
   search(term: string) {
+    if (term === '') {
+      this.searchedUsers = [];
+      return
+    }
     const filtered = this.users.filter((user) => {
       const isName = (user.name.first + user.name.last + user.name.last).includes(term);
       const isEmail = user.email.includes(term);
